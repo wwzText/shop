4798b37053fe24d7ff194477cb4fcba644ec3120
long.wang <long3.wang@symbio.com>
2021-04-11 20:38:41 +0800

    1

--- siteinfo.js
+++ siteinfo.js
@@ -1,7 +1,7 @@
 var siteinfo = {
     'acid': -1,
     'version': '4.3.57',
-    'siteroot': 'https://shop.9026.com/app/index.php',
-    'apiroot': 'https://shop.9026.com/web/index.php?_mall_id=21987',
+    'siteroot': 'https://t12.9026.com/app/index.php',
+    'apiroot': 'https://t12.9026.com/web/index.php?_mall_id=21989',
 };
 module.exports = siteinfo;
